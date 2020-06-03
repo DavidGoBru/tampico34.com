37b0cb8fb530d9301b10690f29c23274dd2c4a29
DavidGoBru <David.gomezbruno@alumno.joyfe.es>
2020-06-03 11:27:12 +0200

    Version david Chrome

--- public/partials/menu.php
+++ public/partials/menu.php
@@ -2,18 +2,18 @@
 		<div class="col-4 px-5 mt-md-3">
 			<img src="images/Logo Tampico34_ espacio de coworking en Madrid.png" alt="Tampico 34 - Espacio de coworking en Madrid">
 		</div>
-<div class="col-8 mt-md-4 mt-2 text-right navbar-expand-md">
-	<nav class="navbar navbar-light" style="display: flex; justify-content: right">
-		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		</button>
-	</nav>
-	<div class="collapse navbar-collapse float-right" id="navbarToggleExternalContent">
-		<div>
-			<a data-scroll class="nav-link float-md-left" href="#Caracteristicas">Características</a>
-			<a class="nav-link float-md-left" href="#Oficinas">Oficinas</a>
-			<a class="nav-link float-md-left" href="#Precios">Precios</a>
-			<a class="nav-link float-md-left" href="#Contacto">Contacto</a>
- 		</div>
+	<div class="col-8 mt-md-4 mt-2 text-right navbar-expand-md">
+		<nav class="navbar navbar-light" style="display: flex; justify-content: right; position: absolute; right: 0; margin-right: 15px">
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+		</nav>
+		<div class="collapse navbar-collapse float-right mt-md-3 mt-5" id="navbarToggleExternalContent">
+			<div>
+				<a data-scroll class="nav-link float-md-left" href="#Caracteristicas">Características</a>
+				<a class="nav-link float-md-left" href="#Oficinas">Oficinas</a>
+				<a class="nav-link float-md-left" href="#Precios">Precios</a>
+				<a class="nav-link float-md-left" href="#Contacto">Contacto</a>
+	 		</div>
 	</div>
 </div>
